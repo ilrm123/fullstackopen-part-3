175304f380ae18594ed654197b51865bd6f5b7d7
Ilari Matikainen <ilari.matikainen@rokki.net>
2022-12-29 20:29:13 +0200

    loput osan 3 tehtävistä

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 var morgan = require('morgan')
 const cors = require('cors')
+const Person = require('./models/person')
 
 app.use(cors())
 
@@ -19,82 +21,109 @@ app.use(morgan(function (tokens, req, res) {
   ].join(' ')
 }))
 
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
+
 let persons = [
-    { 
-      "name": "Arto Hellas", 
-      "number": "040-123456",
-      "id": 1
-    },
-    { 
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523",
-      "id": 2
-    },
-    { 
-      "name": "Dan Abramov", 
-      "number": "12-43-234345",
-      "id": 3
-    },
-    { 
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122",
-      "id": 4
-    }
-  ]
+  {
+    'name': 'Arto Hellas',
+    'number': '040-123456',
+    'id': 1
+  },
+  {
+    'name': 'Ada Lovelace',
+    'number': '39-44-5323523',
+    'id': 2
+  },
+  {
+    'name': 'Dan Abramov',
+    'number': '12-43-234345',
+    'id': 3
+  },
+  {
+    'name': 'Mary Poppendieck',
+    'number': '39-23-6423122',
+    'id': 4
+  }
+]
 
-  let info = `Phonebook has info for ${persons.length} people<br>${new Date()}</br>`
+let info = `Phonebook has info for ${persons.length} people<br>${new Date()}</br>`
 
-  app.get('/', (request, response) => {
-    response.send('<h1>Hello World!</h1>')
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/persons', (request, response) => {
+  Person.find({}).then(people => {
+    info = `Phonebook has info for ${people.length} people<br>${new Date()}</br>`
+    response.json(people)
   })
-  
-  app.get('/api/persons', (request, response) => {
-    response.json(persons)
-  })
+})
 
-  app.get('/info', (request, response) => {
-    response.send(info)
-  })
+app.get('/info', (request, response) => {
+  response.send(info)
+})
 
-  app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
-    
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id).then(person => {
     if (person) {
-        response.json(person)
+      response.json(person)
     } else {
-        response.status(404).end()
+      response.status(404).end()
     }
-  })  
-
-  app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
-  
-    response.status(204).end()
   })
+    .catch(error => next(error))
+})
 
-  app.post('/api/persons', (request, response) => {
-    const person = request.body
-    person.id = Math.floor(Math.random() * 1000)
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+      console.log(result)
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
 
-    if (person.name && person.number) {
-      if (persons.some(p => p.name === person.name)) {
-        response.status(400)
-        response.send('name must be unique')
-      } else if (persons.some(p => p.number === person.number)) {
-        response.status(400)
-        response.send('number must be unique')
-      } else {
-        persons = persons.concat(person)
-        response.json(person)
-      }
-    } else {
+app.post('/api/persons', (request, response, next) => {
+  const body = request.body
+  body.id = Math.floor(Math.random() * 1000)
+
+  if (body.name && body.number) {
+    if (persons.some(p => p.name === body.name)) {
       response.status(400)
-      response.send('name and number required')
+      response.send('name must be unique')
+    } else if (persons.some(p => p.number === body.number)) {
+      response.status(400)
+      response.send('number must be unique')
+    } else {
+      const person = new Person({
+        id: body.id,
+        name: body.name,
+        number: body.number,
+      })
+
+      person.save().then(savedPerson => {
+        response.json(savedPerson)
+      })
+        .catch(error => next(error))
     }
-  
-  })
+  } else {
+    response.status(400)
+    response.send('name and number required')
+  }
+
+})
 
 const PORT = process.env.PORT || 8080
 app.listen(PORT, () => {
--- mongo.js
+++ mongo.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose')
 
 if (process.argv.length<3) {
-    console.log('give password as argument')
-    process.exit(1)
-  }
+  console.log('give password as argument')
+  process.exit(1)
+}
 
 
 const password = process.argv[2]
@@ -16,28 +16,29 @@ const url = `mongodb+srv://ilrm321:${password}@cluster0fullstack.1dkpxsm.mongodb
 mongoose.connect(url)
 
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: String
+  name: String,
+  number: String
 })
 
 const Person = mongoose.model('Person', personSchema)
 
-if (process.argv.length!=3) {
-    const person = new Person({
-        name: name,
-        number: number
-    })
+if (process.argv.length!==3) {
+  const person = new Person({
+    name: name,
+    number: number
+  })
 
-    person.save().then(result => {
-        console.log(`added ${name} number ${number} to phonebook`)
-        mongoose.connection.close()
-    })
+  person.save().then(result => {
+    console.log(result)
+    console.log(`added ${name} number ${number} to phonebook`)
+    mongoose.connection.close()
+  })
 } else {
-    console.log('phonebook:')
-    Person.find({}).then(result => {
-        result.forEach(person => {
-            console.log(`${person.name} ${person.number}`)
-        })
-        mongoose.connection.close()
+  console.log('phonebook:')
+  Person.find({}).then(result => {
+    result.forEach(person => {
+      console.log(`${person.name} ${person.number}`)
     })
+    mongoose.connection.close()
+  })
 }
